c930351731d24ac3c3584e39d6b9956ccdb31062
williamelliott <ironmaiden229@hotmail.com>
2023-03-17 12:15:39 -0400

    project finished

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+function saturdayFun(activity) {
+    if(activity === undefined) {
+        activity = "roller-skate"
+    }
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity) {
+    if(activity === undefined) {
+        activity = "go to the office"
+    }
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(activity) {
+    if(activity === "||") {
+        activity = "||a dedicated programmer||"
+    } else if(activity === "*") {
+        activity = "*a hard worker*"
+    }
+    return `You are ${activity}!`
+}
+
+function wrapAdjective(special) {
+    return function result(adjective) {
+        let sentence = `You are ${special}${adjective}${special}!`
+        return sentence;
+    }
+}
